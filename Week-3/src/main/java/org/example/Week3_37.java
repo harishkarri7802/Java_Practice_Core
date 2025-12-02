e4e9310f0278b3340fc278032c0636b93acb8ce5
harishkarri7802 <harish.karri.sy@outlook.com>
2025-12-01 21:12:41 -0800

    add week 3 programs

---
+++ Week-3/src/main/java/org/example/Week3_37.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//Find the maximum product of two elements in an array.
+public class Week3_37 {
+    public static void main(String[] args) {
+      int[] arrayOne = ArrayOperation.getArrayInputFromUser();
+      ArrayOperation.findMaximumProductOfTwoNumber(arrayOne);
+    }
+}
---
+++ Week-3/src/main/java/org/example/Week3_38.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//Find the maximum product of three elements in an array.
+public class Week3_38 {
+    public static void main(String[] args) {
+         int[] arrayOne = ArrayOperation.getArrayInputFromUser();
+         ArrayOperation.findMaximumProductOfThreeNumber(arrayOne);
+    }
+}
