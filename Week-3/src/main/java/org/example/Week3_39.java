32dc4330fc2bc5bfc2b8e4b0d66752e38d0b1531
harishkarri7802 <harish.karri.sy@outlook.com>
2025-12-01 21:23:30 -0800

    implement remain programs

---
+++ Week-3/src/main/java/org/example/Week3_39.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//Check if an array is sorted in ascending order.
+public class Week3_39 {
+    public static void main(String[] args) {
+        int[] arrayOne = ArrayOperation.getArrayInputFromUser();
+        ArrayOperation.isArrayIsSortedInASC(arrayOne);
+    }
+}
---
+++ Week-3/src/main/java/org/example/Week3_40.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//Check if an array is sorted in descending order.
+public class Week3_40 {
+    public static void main(String[] args) {
+      int[] arrayOne = ArrayOperation.getArrayInputFromUser();
+      ArrayOperation.isArrayIsSortedInDSC(arrayOne);
+    }
+}
