fbea448c046a4b1250e6c9dda486bf4fd30c0e7c
harishkarri7802 <harish.karri.sy@outlook.com>
2025-12-02 21:50:01 -0800

    Delete Week04_01 directory

--- Week04_01/src/main/java/org/example/Week04_25.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-//Longest Common Prefix: Find the longest common prefix among an array of strings.
-public class Week04_25 {
-    public static void main(String[] args) {
-      String[] arrayString = StringOperation.getStringInputFromUser();
-        System.out.println("longest common prefix : " + StringOperation.findLongestCommonPrefixWithinString(arrayString));
-    }
-}
--- Week04_01/src/main/java/org/example/Week04_46.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-//Replace Digits with Hashes: Use regex to replace all digits in a string with #.
-public class Week04_46 {
-    public static void main(String[] args) {
-        String stringValue =Common.getStringNumbers("Enter the string and numbers : ");
-        StringOperation.regexToReplaceAllDigitInStringWithHash(stringValue);
-    }
-}
