33710ed8779fe0b28a854c3e7441d5fa53d0cd5f
harishkarri7802 <harish.karri.sy@outlook.com>
2025-12-02 21:48:16 -0800

    implement programs 1-21,40-45,48,49,46

---
+++ Week-4/src/main/java/org/example/Week04_23.java
@@ -0,0 +1,8 @@
+package org.example;
+//Character Occurrence Check: Write a program to count the number of times a specific character appears in a string.
+public class Week04_23 {
+    public static void main(String[] args) {
+        String valueOne  = Common.getValidString("Enter a string : ");
+        StringOperation.countNumberOfCharacterInString(valueOne);
+    }
+}
