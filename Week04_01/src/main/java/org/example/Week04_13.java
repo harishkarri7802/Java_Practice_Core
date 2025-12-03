7d811016e9d406637f796b58344e26122d4262ca
harishkarri7802 <harish.karri.sy@outlook.com>
2025-12-02 21:26:49 -0800

    implement programs 1-21,40-45,48,49,46

--- Week04_01/src/main/java/org/example/Week04_13.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-//Count Words: Count the number of words in a string.
-public class Week04_13 {
-    public static void main(String[] args) {
-        String sentence  = Common.getValidString("Enter the any sentence : ");
-        System.out.println("number of words in the sentence : " + StringOperation.countNumberOfWordsInString(sentence));
-    }
-}
--- Week04_01/src/main/java/org/example/Week04_16.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-//String Rotation: Write a program to check if one string is a rotation of another string.
-public class Week04_16 {
-    public static void main(String[] args) {
-
-    }
-}
